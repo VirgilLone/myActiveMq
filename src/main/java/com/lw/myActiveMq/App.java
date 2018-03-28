5d3ee4ef0b8f436e97d1d9fdfed1b462b7d2ca95
luo.wen <wyluowen@163.com>
2018-03-29 00:21:35 +0800

    ActiveMq

---
+++ src/main/java/com/lw/myActiveMq/App.java
@@ -0,0 +1,13 @@
+package com.lw.myActiveMq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
