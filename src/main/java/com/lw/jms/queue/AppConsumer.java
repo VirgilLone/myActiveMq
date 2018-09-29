120bcb15a49dba34af04679cf653abbd9720c5fd
luowen <jjg5212@dingtalk.com>
2018-09-29 11:20:43 +0800

    master提交

--- src/main/java/com/lw/jms/queue/AppConsumer.java
+++ src/main/java/com/lw/jms/queue/AppConsumer.java
@@ -17,10 +17,8 @@ import javax.jms.TextMessage;
  * Created by WYluo on 2018/3/28.
  */
 public class AppConsumer {
+    //master合并消失？
     private static final String url ="tcp://192.168.2.116:61616";
-
-    //开发分支 新加内容
-    char fuck='X';
 
     private static final String queueNam ="queue_test";
 
